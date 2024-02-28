376efae4fd066567d95207423954218d0c809f87
Hassan Aziz Tariq <hassantariq64@hotmail.com>
2024-02-29 01:13:34 +0500

    resolved conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 // import { StudentListComponent } from './student-list/student-list.component';
 import { StudentComponent } from './student/student.component';
 import { AddStudentComponent } from './add-student/add-student.component';
-import { ViewstudentsComponent} from './viewstudents/viewstudents.component'
+import { ViewstudentsComponent } from './viewstudents/viewstudents.component'
 
 const routes: Routes = [
   { path: '', component: StudentComponent },
   { path: 'add-student', component: AddStudentComponent },
-  {path: 'view-students-data', component:ViewstudentsComponent}
+  { path: 'view-students-data', component: ViewstudentsComponent }
 ];
 
 @NgModule({
